bc10a9e7cc71d6e8692e21b78ed9ff6736abba8e
dblock <dblock@dblock.org>
2013-02-11 16:51:17 -0500

    Repro for #2812: HATBM relationship count cannot be executed after an object is destroyed.

---
+++ spec/dblock/2812_spec.rb
@@ -0,0 +1,26 @@
+require "spec_helper"
+
+module Test_2812
+  class Sandwich
+    include Mongoid::Document
+    has_and_belongs_to_many :meats
+  end
+
+  class Meat
+    include Mongoid::Document
+    has_and_belongs_to_many :sandwiches
+  end
+end
+
+describe "2812" do
+
+  it "issue" do
+    # create a meats collection, without it, no error
+    Test_2812::Meat.create!
+
+    sandwich = Test_2812::Sandwich.create!
+    sandwich.destroy
+    sandwich.meats.count.should == 0
+  end
+
+end
